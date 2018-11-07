1203f0ad387b841d1be60369380c93a91f2eb2eb
arie.cattan <arie.cattan@gmail.com>
2018-11-07 18:43:08 +0200

    qMLE

---
+++ code/MLETrain.py
@@ -0,0 +1,37 @@
+from utils import *
+
+
+def QMLE(fname):
+    train = read_data(fname)
+    Q_trigram = {}
+    Q_bigram = {}
+    Q_unigram = {}
+
+    for line in train:
+        line = map(lambda x: x[1], line)
+        for a, b, c in zip(line, line[1:], line[2:]):
+            Q_unigram[a] = Q_unigram.get(a, 0) + 1
+            Q_bigram[a + " " + b] = Q_bigram.get(a + " " + b, 0) + 1
+            Q_trigram[a + " " + b + " " + c] = Q_trigram.get(a + " " + b + " " + c, 0) + 1
+
+        Q_bigram[b + " " + c] = Q_bigram.get(b + " " + c, 0) + 1
+        Q_unigram[b] = Q_unigram.get(b, 0) + 1
+        Q_unigram[c] = Q_unigram.get(c, 0) + 1
+
+    return Q_unigram, Q_bigram, Q_trigram
+
+
+def qFile(fname):
+    Q_unigram, Q_bigram, Q_trigram = QMLE(fname)
+    data = []
+    for key, label in Q_unigram.items():
+        data.append(key + "\t" + str(label))
+    for key, label in Q_bigram.items():
+        data.append(key + "\t" + str(label))
+    for key, label in Q_trigram.items():
+        data.append(key + "\t" + str(label))
+    write_to_file("q.mle", data)
+    return data
+
+
+a, b, c = QMLE("../data/ass1-tagger-train")
--- code/utils.py
+++ code/utils.py
@@ -2,11 +2,100 @@
 # Use it if you want, or ignore it.
 import random
 import numpy as np
+import nltk
 
 
 def read_data(fname):
-    data = {}
+    data = []
     for line in file(fname):
-        arr = line.split()
-        data.extend(map(lambda x: x.split('/'), arr))
+        arr = ['***/STR', '***/STR']
+        arr.extend(line.split(" "))
+        data.append(map(lambda x: x.split('/'), arr))
     return data
+
+def write_to_file(fname, data):
+    np.savetxt(fname, data, delimiter='\n')
+
+
+VBD
+PRP$
+CCL
+WDT
+JJ
+WP
+RP
+$
+:
+(
+FW
+,
+RB
+WAD
+NNS
+NNP
+WRB
+T
+NN
+SYM
+NNS
+SYM
+EMI
+EMS
+E
+Net
+#
+VBN
+PC
+IN
+DT
+S
+EX
+MD
+VMS
+UH
+UA
+.
+VBG
+VBD
+VBN
+VBP
+VBZ
+NN
+CD
+.
+)
+JJ
+:
+VBP
+PRP
+``
+B
+CC
+VB
+PDT
+CD
+NNPS
+WP$
+JJS
+JJR
+NNPS
+''
+,
+''
+RB
+DT
+)
+POS
+TO
+LS
+NNP
+VBZ
+VB
+ABC
+JJR
+``
+RBS
+RBR
+STR
+IN
+RP
